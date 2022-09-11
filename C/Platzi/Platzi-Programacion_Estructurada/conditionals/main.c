bc4c96b9fed58085972714d680f78342866367be
CristopherVanegas <cristopher.platzi@gmail.com>
2022-09-11 10:54:06 -0700

    ADDED conditionals

---
+++ C/Platzi/Platzi-Programacion_Estructurada/conditionals/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Condicion if\n");
+
+    float valA, valB, valC;
+    valA = valB = 100;
+
+    printf("Primera condicion: ");
+
+    if (valA == valB)
+        printf("Ambos son iguales \n");
+
+    printf("Segunda condición: ");
+    if (valA == valB) {
+        printf("Ambos son iguales \n");
+        valC = valA + valB;
+        printf("Ademas la suma de ambos numeros es: %f", valC);
+    }
+    return 0;
+}
