34e89fa59773ac7241b0077902555fc8269d76c1
CristopherVanegas <cristopher.platzi@gmail.com>
2022-09-16 21:51:11 -0700

    ADDED while loop

---
+++ C/Platzi/Platzi-Programacion_Estructurada/bucleWhile/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Iterador While");
+
+    int limit;
+        scanf("%i", &limit);
+
+    int i = 1;
+    while (i <= limit) {
+        printf("Numero: %i\n", i);
+        i++;
+    }
+
+    return 0;
+}
