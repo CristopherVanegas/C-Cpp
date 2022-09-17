eaadd18b98cbca367e69767ea27f32cc4118fcf4
CristopherVanegas <cristopher.platzi@gmail.com>
2022-09-16 21:48:03 -0700

    Added conditionals

--- C/Platzi/Platzi-Programacion_Estructurada/conditionals/main.c
+++ C/Platzi/Platzi-Programacion_Estructurada/conditionals/main.c
@@ -3,7 +3,13 @@
 
 int main()
 {
-    printf("Condicion if\n");
+    condicionales_1();
+    return 0;
+}
+
+
+void condicionales_1() {
+        printf("Condicion if\n");
 
     float valA, valB, valC;
     valA = valB = 100;
@@ -19,5 +25,4 @@ int main()
         valC = valA + valB;
         printf("Ademas la suma de ambos numeros es: %f", valC);
     }
-    return 0;
 }
