343000cf20488ed3de619478308d4d7670d45d0f
Cristopher Vanegas <cristopher.platzi@gmail.com>
2022-11-15 21:39:26 -0500

    ADDED String exercise and challenge classs, looping and printing.

---
+++ C/Platzi/Platzi-Programacion_Estructurada/string/string.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    classChallenge();
+    return 0;
+}
+
+void classChallenge() {
+    printf("Cadena de caracteres\n");
+    char nameC[50];
+    int size;
+    printf("Ingregar nombre y obtener valor usando la funcion fgets(): ");
+    fgets(nameC, 50, stdin);
+
+    size = strlen(nameC);
+    printf("\nEl size de la cadena es: %i\n", size-1);
+
+    printf("El nombre invertido es: \n");
+    for (int i = size-1; i >= 0; i--)
+    {
+        printf("%c", nameC[i]);
+    }    
+
+    return nameC;
+}
+
+void classExercise() {
+    printf("Cadena de caracteres\n");
+    char nameC[50];
+    int size;
+    printf("Ingregar nombre y obtener valor usando la funcion fgets(): ");
+    fgets(nameC, 50, stdin);
+
+    printf("El nombre es: ");
+    puts(nameC);
+
+    size = strlen(nameC);
+    printf("\nEl size de la cadena es: %i\n", size-1);
+    
+    return nameC;
+}
+
