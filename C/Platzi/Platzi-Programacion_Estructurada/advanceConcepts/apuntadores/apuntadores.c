f5c88d251885387b71e66a19b9ce4b19c887aa63
Cristopher Vanegas <cristopher.platzi@gmail.com>
2022-11-21 10:28:52 -0500

    ADDED apuntadores / pointers exercise

---
+++ C/Platzi/Platzi-Programacion_Estructurada/advanceConcepts/apuntadores/apuntadores.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Apuntadores!\n");
+    
+    float val;                  // val = 0.000000
+    float *ApuntadorVal;
+
+    ApuntadorVal = &val;
+    *ApuntadorVal = 3.1416;     // val = 3.1416
+    printf("%f", val);
+    return 0;
+}
